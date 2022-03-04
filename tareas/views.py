8731445ad17b35c3c4a7bfa72b5a1e53fb705026
Diego Carnero <you@example.com>
2022-03-04 19:58:16 +0100

    Primeros pasos para ejecutar y ver algo

--- tareas/views.py
+++ tareas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Tarea
 
 # Create your views here.
+
+'''
+Se añade un import a la clase de tu modelo
+'''
+def listaTareas(request):
+    return render(request,'listaActivos.html',{'tareas':Tarea.objects.all()})
