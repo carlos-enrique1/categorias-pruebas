f4750a9b1c52cc1ac662644cf28b9604958d4a59
Carlos Martinez <carlos>
2022-03-04 19:19:41 +0100

    Crear la base de datos

--- tareas/models.py
+++ tareas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+# Creamos el modelo de la base de datos
+
+class Tarea (models.Model):
+    '''
+    Aqui creamos los campos de la Base de Datos
+    '''
+    nombre=models.CharField(max_length=150,blank=False)
+    categoria=models.CharField(max_length=150,blank=False)
+    estado=models.BooleanField(blank=False)
+    
+    def __str__(self) -> str:
+        '''
+        Esta función devuelve una cadena de texto 
+        '''
+        return super().__str__()
